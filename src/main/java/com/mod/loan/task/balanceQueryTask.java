ade6f9e902508df129e6c0c06b35a32cb22bfef6
duanyi <d5278779@163.com>
2019-04-26 18:26:55 +0800

    易宝查询商户余额 自动任务

---
+++ src/main/java/com/mod/loan/task/balanceQueryTask.java
@@ -0,0 +1,84 @@
+package com.mod.loan.task;
+
+import com.mod.loan.common.enums.ResponseEnum;
+import com.mod.loan.common.message.QueueSmsMessage;
+import com.mod.loan.common.model.ResultMessage;
+import com.mod.loan.config.rabbitmq.RabbitConst;
+import com.mod.loan.model.Merchant;
+import com.mod.loan.service.MerchantService;
+import com.mod.loan.service.YeepayService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("balanceTest")
+public class balanceQueryTask {
+
+    private static final Logger logger = LoggerFactory.getLogger(balanceQueryTask.class);
+
+    private final YeepayService yeepayService;
+    private final MerchantService merchantService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public balanceQueryTask(YeepayService yeepayService, MerchantService merchantService, RabbitTemplate rabbitTemplate) {
+        this.yeepayService = yeepayService;
+        this.merchantService = merchantService;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Scheduled(cron = "0 0 10 * * ?")
+    public void MerchantBalanceQueryTask() {
+        try {
+            logger.info("------------------balanceQueryTask start------------------");
+            List<Merchant> merchantList = merchantService.selectAll();
+            for (Merchant merchant : merchantList) {
+                int bindType = merchant.getBindType();
+                switch (bindType){
+//                    case 1://合利宝
+//                        break;
+//                    case 2://富友
+//                        break;
+//                    case 3://汇聚
+//                        break;
+                    case 4://易宝
+                        StringBuffer balance = new StringBuffer();
+                        String errMsg = yeepayService.balanceQuery(merchant.getYeepay_loan_appkey(),merchant.getYeepay_loan_private_key(),balance);
+                        if (errMsg==null){
+                            QueueSmsMessage smsMessage = new QueueSmsMessage();
+                            smsMessage.setClientAlias(merchant.getMerchantAlias());
+                            smsMessage.setType("2004");
+                            smsMessage.setPhone("15757127746,13979127403,18072878602");
+                            smsMessage.setParams(balance.toString());
+                            rabbitTemplate.convertAndSend(RabbitConst.queue_sms, smsMessage);
+                        }else {
+                            logger.error("yeepay appkey={} balanceQuery error={}", merchant.getYeepay_loan_appkey(), errMsg);
+                        }
+                        break;
+                    default:
+                        logger.error("bindType = {} unsupport", bindType);
+                        break;
+                }
+                Thread.sleep(100);
+            }
+        } catch (Exception e) {
+            logger.error("商户余额查询异常={}", e);
+        }
+
+        logger.info("------------------balanceQueryTask end--------------------");
+    }
+
+
+    @RequestMapping(value = "query")
+    public ResultMessage balance_query_test() {
+        MerchantBalanceQueryTask();
+        return new ResultMessage(ResponseEnum.M2000);
+    }
+}
---
+++ src/main/java/com/mod/loan/util/heli/vo/response/QueryMerchantAccountResponseVo.java
@@ -0,0 +1,124 @@
+package com.mod.loan.util.heli.vo.response;
+
+/**
+ * Created by heli50 on 2017/4/15.
+ */
+public class QueryMerchantAccountResponseVo {
+    private String rt1_bizType;
+    private String rt2_retCode;
+    private String rt3_retMsg;
+    private String rt4_customerNumber;
+    private String rt5_accountStatus;
+    private String rt6_balance;
+    private String rt7_frozenBalance;
+    private String rt8_d0Balance;
+    private String rt9_T1Balance;
+    private String rt10_currency;
+    private String rt11_createDate;
+    private String rt12_desc;
+    private String sign;
+
+    public String getRt1_bizType() {
+        return rt1_bizType;
+    }
+
+    public void setRt1_bizType(String rt1_bizType) {
+        this.rt1_bizType = rt1_bizType;
+    }
+
+    public String getRt2_retCode() {
+        return rt2_retCode;
+    }
+
+    public void setRt2_retCode(String rt2_retCode) {
+        this.rt2_retCode = rt2_retCode;
+    }
+
+    public String getRt3_retMsg() {
+        return rt3_retMsg;
+    }
+
+    public void setRt3_retMsg(String rt3_retMsg) {
+        this.rt3_retMsg = rt3_retMsg;
+    }
+
+    public String getRt4_customerNumber() {
+        return rt4_customerNumber;
+    }
+
+    public void setRt4_customerNumber(String rt4_customerNumber) {
+        this.rt4_customerNumber = rt4_customerNumber;
+    }
+
+    public String getRt5_accountStatus() {
+        return rt5_accountStatus;
+    }
+
+    public void setRt5_accountStatus(String rt5_accountStatus) {
+        this.rt5_accountStatus = rt5_accountStatus;
+    }
+
+    public String getRt6_balance() {
+        return rt6_balance;
+    }
+
+    public void setRt6_balance(String rt6_balance) {
+        this.rt6_balance = rt6_balance;
+    }
+
+    public String getRt7_frozenBalance() {
+        return rt7_frozenBalance;
+    }
+
+    public void setRt7_frozenBalance(String rt7_frozenBalance) {
+        this.rt7_frozenBalance = rt7_frozenBalance;
+    }
+
+    public String getRt8_d0Balance() {
+        return rt8_d0Balance;
+    }
+
+    public void setRt8_d0Balance(String rt8_d0Balance) {
+        this.rt8_d0Balance = rt8_d0Balance;
+    }
+
+    public String getRt9_T1Balance() {
+        return rt9_T1Balance;
+    }
+
+    public void setRt9_T1Balance(String rt9_T1Balance) {
+        this.rt9_T1Balance = rt9_T1Balance;
+    }
+
+    public String getRt10_currency() {
+        return rt10_currency;
+    }
+
+    public void setRt10_currency(String rt10_currency) {
+        this.rt10_currency = rt10_currency;
+    }
+
+    public String getRt11_createDate() {
+        return rt11_createDate;
+    }
+
+    public void setRt11_createDate(String rt11_createDate) {
+        this.rt11_createDate = rt11_createDate;
+    }
+
+    public String getRt12_desc() {
+        return rt12_desc;
+    }
+
+    public void setRt12_desc(String rt12_desc) {
+        this.rt12_desc = rt12_desc;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
