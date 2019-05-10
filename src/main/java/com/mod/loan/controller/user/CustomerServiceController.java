b60a6c13786e602df348140a14307003db1987fe
duanyi <d5278779@163.com>
2019-05-10 17:55:39 +0800

    客服电话

--- src/main/java/com/mod/loan/controller/user/CustomerServiceController.java
+++ src/main/java/com/mod/loan/controller/user/CustomerServiceController.java
@@ -3,6 +3,7 @@ package com.mod.loan.controller.user;
 import com.mod.loan.common.annotation.Api;
 import com.mod.loan.common.annotation.LoginRequired;
 import com.mod.loan.common.enums.ResponseEnum;
+import com.mod.loan.common.model.RequestThread;
 import com.mod.loan.common.model.ResultMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,24 @@ import java.util.Map;
 @RequestMapping(value = "customer_service")
 public class CustomerServiceController {
 
+	private HashMap<String, String> telMap= new HashMap<>();
+	{
+		telMap.put("mx","13282833651");
+		telMap.put("dawang","13282833651");
+		telMap.put("huijie","13282833651");
+		telMap.put("haitun","057128064320");
+	}
 	/**
 	 * 客服信息查询
 	 */
 	@Api
-	@LoginRequired(check = true)
+	@LoginRequired()
 	@RequestMapping(value = "/info")
 	public ResultMessage getInfo() {
+		String clientAlias = RequestThread.getClientAlias();
 		Map<String, Object> data = new HashMap<>();
 		//data.put("qq", "2513171881");
-		data.put("tel","13282833651");
+		data.put("tel",telMap.get(clientAlias));
 		return new ResultMessage(ResponseEnum.M2000, data);
 	}
 
