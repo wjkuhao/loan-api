a41ffd05119a2843f829e60543746f3c16b94a01
duanyi <d5278779@163.com>
2019-05-15 11:30:34 +0800

    对接贷超，返回初审通过提示信息

--- src/main/java/com/mod/loan/controller/order/LoanOrderController.java
+++ src/main/java/com/mod/loan/controller/order/LoanOrderController.java
@@ -183,9 +183,9 @@ public class LoanOrderController {
 			loanBefore.setEventDescribe(String.format("申请周转资金%s元，周期%s天，服务费%s元，到账%s元", order.getBorrowMoney(),order.getBorrowDay(),order.getTotalFee(),order.getActualMoney()));
 
 			LoanBefore loanBefore2 = new LoanBefore();
-			loanBefore2.setEvent("抱歉，你没有通过审核");
-			loanBefore2.setEventTime(TimeUtils.parseTime(order.getAuditTime(),TimeUtils.dateformat0));
-			loanBefore2.setEventDescribe("距下次可申请借款时间");
+            loanBefore2.setEvent("初审通过");
+            loanBefore2.setEventTime(TimeUtils.parseTime(order.getAuditTime(),TimeUtils.dateformat0));
+            loanBefore2.setEventDescribe("小胖墩、飞金鱼等40+优质资金方可供您借款，点击详情查看");
 
 			loanBeforeList.add(loanBefore);
 			loanBeforeList.add(loanBefore2);
